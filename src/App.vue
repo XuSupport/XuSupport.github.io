8b93d08b91459309bcda5696b4313fab57968950
xusupport <1102663302@qq.com>
2024-11-22 23:22:27 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
   <div>
-    <div class="bgBox" v-motion :initial="{ opacity: 0, y: 100 }" :enter="{ opacity: 2, y: 0 }" :duration="1000">
-      <img class="circular-image" src="/images/pic1.jpg" alt="">
+    <div class="top-banner">
+      <img src="/images/logo.png" alt="图标" class="banner-icon">
+      <span class="banner-text">浙江工业大学之江学院网络与信息安全社团</span>
     </div>
+    <div class="bg-container">
+      <!-- 动态生成气泡 -->
+      <div v-for="bubble in bubbles" :key="bubble.id" class="bubble" :style="bubbleStyle(bubble)"></div>
 
+      <!-- 图形部分 -->
+<!--      <div-->
+<!--          v-for="(image, index) in images"-->
+<!--          :key="index"-->
+<!--          class="bgBox"-->
+<!--          :style="getEdgeStyle()"-->
+<!--          v-motion-->
+<!--          :initial="{ opacity: 0, y: 100 }"-->
+<!--          :enter="{ opacity: 1, y: 0 }"-->
+<!--          :duration="1000"-->
+<!--      >-->
+<!--        <img class="circular-image" :src="image.src" alt="" />-->
+<!--      </div>-->
+    </div>
 
     <div class="bgGrid">
       <div class="Grid" v-motion :initial="{ opacity: 0, y: -25 }" :enter="{ opacity: 1, y: 0 }" :duration="1000">
@@ -17,7 +35,7 @@
     <div class="main" v-motion :initial="{ opacity: 0 }" :enter="{ opacity: 1 }" :duration="1000">
       <div class="info">
         <div class="header">
-          <img src="/images/logo.png" alt="">
+          <img src="/images/logo.png" alt=""/>
         </div>
 
         <div class="infoText">
@@ -29,253 +47,286 @@
       <div class="typewriter">
         <i class="iconfont icon-baojiaquotation2"></i>
         <VueTyped :strings="typingTexts" :startDelay="300" :typeSpeed="100" :backSpeed="30" :loop="true"
-                  :showCursor="true">
-        </VueTyped>
+                  :showCursor="true"/>
         <i class="iconfont icon-baojiaquotation"></i>
       </div>
 
       <div class="btns">
-        <a v-for="i in btnList" :key="i.animate" :href="i.href" target="_blank">
-          <vs-button type="gradient" :color="i.color" animation-type="scale">
-            <i :class="`iconfont ${i.icon}`"></i>
-
+        <a v-for="item in btnList" :key="item.animate" :href="item.href" target="_blank">
+          <vs-button type="gradient" :color="item.color" animation-type="scale">
+            <i :class="`iconfont ${item.icon}`"></i>
             <template #animate>
-              {{ i.animate }}
+              {{ item.animate }}
             </template>
           </vs-button>
         </a>
 
         <vs-button class="lastBtn" color="#457B9D" animation-type="scale" @click="active = true">
           <i class="iconfont icon-guanyu"></i>
-
-          <template #animate>
-            关于
-          </template>
+          <template #animate>关于</template>
         </vs-button>
       </div>
     </div>
 
-    <div class="footer">
-      By 之江网安社团 | ©2024
-    </div>
+    <!-- Footer -->
+    <div class="footer">By 之江网安社团 | ©2024</div>
 
+    <!-- Dialog -->
     <vs-dialog overlay-blur width="550px" not-center v-model="active">
       <template #header>
-        <h2 class="not-margin">
-          关于本站
-        </h2>
+        <h2 class="not-margin">关于本站</h2>
       </template>
-
       <div class="con-content">
-
-        <vs-alert color="#FE8599" type="gradient" v-model:hidden-content="techHidden">
+        <!-- 技术栈警告框 -->
+        <vs-alert color="#FE8599" type="gradient" v-model:visible="techHidden">
           <template #title>技术栈</template>
           <p>本站基于以下技术搭建和以下服务商部署</p>
-
+          <!-- 头像组 -->
           <vs-avatar-group class="aboutAva" float max="8">
-
-            <vs-tooltip placement="top" v-for="i in avaters" :key="i.content">
-              <vs-avatar :color="i.color">
-                <i :class="`iconfont ${i.icon}`"></i>
+            <vs-tooltip placement="top" v-for="item in avaters" :key="item.content">
+              <vs-avatar :color="item.color">
+                <i :class="`iconfont ${item.icon}`"></i>
               </vs-avatar>
-
-              <template #content>{{ i.content }}</template>
+              <template #content>{{ item.content }}</template>
             </vs-tooltip>
 
+            <!-- 固定的 CDN 项目 -->
             <vs-tooltip placement="top">
               <vs-avatar color="#FF6D1A">
                 初
               </vs-avatar>
-
               <template #content>初七云cdn</template>
             </vs-tooltip>
-
           </vs-avatar-group>
         </vs-alert>
 
-        <vs-alert color="#00BCD4" type="gradient" v-model:hidden-content="aboutHidden">
-          <template #title>关于项目</template>
-
-          <p>你可以从这里访问 <b>我的博客、GitHub、哔哩哔哩、网易云歌单</b> 以及给我发 <b>邮件</b> ！</p>
-          <p>本项目借鉴了
-            <b><a href="https://pzj.us.kg/" target="_blank">pzjawa</a></b> 、
-            <b><a href="https://www.liushen.fun/" target="_blank">清羽飞扬</a></b>
-            等主页，感谢他们awa
+        <!-- 关于社团 -->
+        <vs-alert color="#00BCD4" type="gradient" v-model:visible="aboutHidden">
+          <template #title>关于社团的章程</template>
+          <p>你可以从这里访问 <b>GitHub、哔哩哔哩</b> 以及给团队发 <b>邮件</b> ！</p>
+          <p>你可以从这里了解到
+            <b><a href="https://planes-cross-5d3.craft.me/ZW734G08dfLdLZ" target="_blank">社团章程</a></b> 、
+            <b><a href="https://planes-cross-5d3.craft.me/3Soa2s3sCcHyV1" target="_blank">会议记录</a></b>
+            等信息，感谢学长学姐的付出
           </p>
-          <p>项目已经开源：</p>
+          <p>项目借鉴于：</p>
           <p><a href="https://github.com/QNquenan/homepage-for-vue3"
                 target="_blank">https://github.com/QNquenan/homepage-for-vue3</a>
           </p>
         </vs-alert>
       </div>
-
-      <template #footer>
-        <div class="con-footer">
-          <div class="reTheme">
-
-            <input type="radio" id="light" name="theme" :checked="theme == 'light'">
-            <label @click="setTheme('light')" for="light">
-              <i class="iconfont icon-baitian"></i>
-            </label>
-
-            <input type="radio" id="system" name="theme" :checked="theme == 'system'">
-            <label @click="setTheme('system')" for="system">
-              <i class="iconfont icon-gensuixitong"></i>
-            </label>
-
-            <input type="radio" id="dark" name="theme" :checked="theme == 'dark'">
-            <label @click="setTheme('dark')" for="dark">
-              <i class="iconfont icon-yewan"></i>
-            </label>
-
-            <div class="checkedBg"></div>
-          </div>
-
-          <div class="footerBtn">
-            <vs-button color="#fe8599" @click="versions" type="border">
-              当前版本
-            </vs-button>
-            <vs-button color="#fe8599" @click="active = false">
-              知道啦
-            </vs-button>
-          </div>
-        </div>
-      </template>
     </vs-dialog>
   </div>
 </template>
 
-<script>
-import { VsNotification } from 'vuesax-alpha'
+<script setup>
+import {ref, onMounted} from 'vue'
+import {VsNotification} from 'vuesax-alpha'
 
-export default {
-  data() {
-    return {
-      techHidden: true,
-      aboutHidden: true,
-      typingTexts: [
-        "你好鸭，欢迎来到之江网安资源站！",
-        "彼方尚有荣光在，世界不止眼前的苟且，还有诗和远方",
-        "累了可以在我这里歇歇脚嗷",
-        "May you happy every day",
-      ],
-      btnList: [
-        {
-          icon: 'icon-wodeboke',
-          animate: '博客',
-          color: '#fe8599',
-          href: 'https://blog.quenan.love'
-        },
-        {
-          icon: 'icon-github',
-          animate: 'Github',
-          color: '#3d3d3d',
-          href: 'https://github.com/QNquenan'
-        },
-        {
-          icon: 'icon-bilibili',
-          animate: 'BiliBili',
-          color: '#0BA6D8',
-          href: 'https://space.bilibili.com/495882959'
-        },
-        {
-          icon: 'icon-youjian1',
-          animate: 'E-mail',
-          color: '#FACB1E',
-          href: 'mailto:qn2987271942@outlook.com'
-        },
-        {
-          icon: 'icon-wangyiyunyinle1',
-          animate: '网易云',
-          color: '#D81E06',
-          href: 'https://music.163.com/#/playlist?id=2696501847&creatorId=1785635020'
-        },
-      ],
-      avaters: [
-        {
-          color: '#43B884',
-          icon: 'icon-vue',
-          content: 'Vue3'
-        },
-        {
-          color: '#46C93A',
-          icon: 'icon-vuesax-linear-vuesax',
-          content: 'Vuesax for Vue3'
-        },
-        {
-          color: '#1FD0ED',
-          icon: 'icon-less',
-          content: 'Less'
-        },
-        {
-          color: '#FFBA00',
-          icon: 'icon-vite',
-          content: 'Vite'
-        },
-        {
-          color: '#000',
-          icon: 'icon-vercel',
-          content: 'Vercel'
-        },
-        {
-          color: '#000',
-          icon: 'icon-github',
-          content: 'Github'
-        }
-      ],
-      upTime: '2024-09-16',
-      version: 'beta3',
-      gridRows: 9,
-      gridCols: 5,
-      active: false,
-      isDarkMode: true,
-      theme: 'system' // 默认是亮色模式
-    }
-  },
-  mounted() {
-    if (localStorage.getItem('isTheme')) {
-      this.theme = localStorage.getItem('isTheme')
-      this.applyTheme()
-    } else {
-      this.applyTheme()
-    }
-    // 监听系统主题变化
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', this.applyTheme);
-  },
-  methods: {
-    setTheme(mode) {
-      this.theme = mode;
-      localStorage.setItem('isTheme', mode)
-      this.applyTheme();
-    },
-    applyTheme() {
-      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-      const themeToApply = this.theme === 'system' ? (prefersDark ? 'dark' : 'light') : this.theme;
-      document.documentElement.setAttribute('data-theme', themeToApply);
-    },
-    versions() {
-      VsNotification({
-        icon: '<i class="iconfont icon-guanyu"></i>',
-        progressAuto: true,
-        position: 'top-center',
-        title: '当前的版本',
-        color: '#FE8599',
-        content: `现在是 ${this.upTime} 更新的 ${this.version}`
-      })
-    }
+const techHidden = ref(true)
+const aboutHidden = ref(true)
+const active = ref(false)
+const theme = ref('system')
+
+const images = ref([
+  { src: '/images/pic1.jpg' },
+  { src: '/images/pic2.jpg' },
+  { src: '/images/pic3.jpg' },
+  { src: '/images/pic4.jpg' },
+])
+
+// 生成气泡数据
+const bubbles = ref(Array.from({ length: 20 }, (_, id) => ({
+  id,
+  size: Math.random() * 50 + 20, // 气泡大小随机 20px - 70px
+  left: Math.random() * 100, // 气泡初始水平位置
+  duration: Math.random() * 10 + 5, // 上升时长随机 5s - 15s
+  delay: Math.random() * 5, // 延迟随机 0s - 5s
+})))
+
+const getEdgeStyle = () => {
+  const edge = Math.floor(Math.random() * 4);
+  const randomOffset = Math.random() * 90;
+  switch (edge) {
+    case 0: return { top: `${randomOffset}%`, left: '10%' }
+    case 1: return { top: `${randomOffset}%`, left: '10%' }
+    case 2: return { top: `${randomOffset}%`, left: '10%' }
+    case 3: return { top: `${randomOffset}%`, left: '10%' }
   }
 }
+
+// 动态气泡样式
+const bubbleStyle = (bubble) => ({
+  width: `${bubble.size}px`,
+  height: `${bubble.size}px`,
+  left: `${bubble.left}%`,
+  animationDuration: `${bubble.duration}s`,
+  animationDelay: `${bubble.delay}s`
+})
+
+// Typing text data
+const typingTexts = [
+  "你好鸭，欢迎来到之江网安资源站！",
+  "彼方尚有荣光在，世界不止眼前的苟且，还有诗和远方",
+  "累了可以在我这里歇歇脚嗷",
+  "May you happy every day"
+]
+
+const avaters = ref([
+  {
+    color: '#43B884',
+    icon: 'icon-vue',
+    content: 'Vue3'
+  },
+  {
+    color: '#46C93A',
+    icon: 'icon-vuesax-linear-vuesax',
+    content: 'Vuesax for Vue3'
+  },
+  {
+    color: '#1FD0ED',
+    icon: 'icon-less',
+    content: 'Less'
+  },
+  {
+    color: '#FFBA00',
+    icon: 'icon-vite',
+    content: 'Vite'
+  },
+  {
+    color: '#000',
+    icon: 'icon-vercel',
+    content: 'Vercel'
+  },
+  {
+    color: '#000',
+    icon: 'icon-github',
+    content: 'Github'
+  }
+])
+
+// Data for button list
+const btnList = [
+  {icon: 'icon-github', animate: 'Github', color: '#3d3d3d', href: 'https://github.com/XuSupport'},
+  {
+    icon: 'icon-bilibili',
+    animate: 'BiliBili',
+    color: '#0BA6D8',
+    href: 'https://b23.tv/D2qCO78'
+  },
+  {
+    icon: 'icon-youjian1',
+    animate: 'E-mail',
+    color: '#FACB1E',
+    href: 'mailto:1102663302@qq.com'
+  }
+
+]
+
+// Apply theme
+onMounted(() => {
+  applyTheme()
+})
+
+const applyTheme = () => {
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches
+  const themeToApply = theme.value === 'system' ? (prefersDark ? 'dark' : 'light') : theme.value
+  document.documentElement.setAttribute('data-theme', themeToApply)
+}
+
 </script>
+
 
 <style lang="less">
 @import url(//at.alicdn.com/t/c/font_4685493_lrpbngzgvbk.css);
-.circular-image {
-  border-radius: 50%;  /* 使图片变成圆形 */
-  border: 5px solid #457B9D;  /* 圆形边框，颜色为 #457B9D，可以根据需要修改 */
-  width: 450px;  /* 设置图片的宽度，根据需要调整 */
-  height: 450px; /* 设置图片的高度，确保是一个正方形，才能保持圆形 */
-  object-fit: cover; /* 确保图片适应圆形边框 */
+
+.bg-container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  background: url('/images/3.jpg') no-repeat center center;
+  background-size: cover; /* 背景图片自动调整尺寸以覆盖整个容器 */
 }
+
+
+
+
+/* 气泡样式 */
+.bubble {
+  position: absolute;
+  bottom: -100px; /* 初始位置在屏幕外底部 */
+  background-color: rgba(173, 216, 230, 0.4); /* 淡蓝色气泡 */
+  border-radius: 50%;
+  animation: floatUp infinite ease-in-out;
+}
+
+/* 气泡上升动画 */
+@keyframes floatUp {
+  0% {
+    transform: translateY(0) scale(0.8); /* 初始较小 */
+    opacity: 0;
+  }
+  50% {
+    opacity: 0.8;
+  }
+  100% {
+    transform: translateY(-100vh) scale(1.2); /* 缩放并上升到顶部 */
+    opacity: 0;
+  }
+}
+
+.bgBox {
+  position: absolute;
+  animation: slowMove 10s infinite alternate ease-in-out;
+}
+
+.circular-image {
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  border: 2px solid rgba(128, 128, 128, 0.3);
+}
+
+/* 图形缓慢移动动画 */
+@keyframes slowMove {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translateY(-20px) translateX(20px);
+  }
+}
+
+/* 顶部横幅样式 */
+.top-banner {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed; /* 固定在页面顶部 */
+  top: 0;
+  left: 0;
+  width: 100%; /* 占满整个页面宽度 */
+  background-color: white; /* 背景白色 */
+  padding: 10px 0; /* 上下内边距 */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* 添加阴影 */
+  z-index: 1000; /* 确保横幅显示在其他元素上方 */
+}
+
+/* 图标样式 */
+.banner-icon {
+  width: 24px; /* 图标宽度 */
+  height: 24px; /* 图标高度 */
+  margin-right: 8px; /* 图标和文字的间距 */
+}
+
+/* 文本样式 */
+.banner-text {
+  font-size: 16px; /* 字体大小 */
+  font-weight: bold; /* 粗体 */
+  color: gray; /* 文字灰色 */
+}
+
 
 /* 阿里巴巴图标库 */
 </style>
